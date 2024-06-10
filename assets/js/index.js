aa8697c4bdc516ea602c980f1ff5bfcfe1ddcab5
Mârius <52293277+mymarius@users.noreply.github.com>
2024-06-11 00:01:02 +0300

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-import { setupPageFunctionality, setupLoginModalFunctionality } from "./domUtils.js";
-import { setupDownloadButton } from "./downloadUtils.js";
-import { setupCounters } from "./counterUtils.js";
-
-setupLoginModalFunctionality()
-setupPageFunctionality();
-setupDownloadButton();
-setupCounters();
